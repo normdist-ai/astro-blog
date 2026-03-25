78f035ad8bcf96f4be8fd751ff20953d2c3cadf7
小瑞 <xiaorui@normdist.com>
2026-03-25 17:18:48 +0800

    Initial Astro blog setup

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Lean Blog';
+export const SITE_DESCRIPTION = '进化概率论 - Lean的技术博客与思考';
